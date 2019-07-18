0fa64b06a4438d156b790b577bfbf821b14c5910
kimseunghyun <kshyun9426@naver.com>
2019-07-18 17:21:36 +0900

    springMVC Practice(코드로 배우는 스프링 웹 프로젝트)

---
+++ SpringJex01/src/main/java/org/zerock/config/RootConfig.java
@@ -0,0 +1,28 @@
+package org.zerock.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class RootConfig {
+	
+	//HikariCP는 빠른 커넥션풀을 제공함
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setUsername("book_ex");
+		hikariConfig.setPassword("book_ex");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		return dataSource;
+	}
+	
+	
+}
---
+++ SpringJex01/src/main/java/org/zerock/config/ServletConfig.java
@@ -0,0 +1,8 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+public class ServletConfig {
+	
+}
