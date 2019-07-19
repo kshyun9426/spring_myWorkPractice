8d448bf1c010cc6a7782fff39c4e374ea85bab78
kimseunghyun <kshyun9426@naver.com>
2019-07-19 15:06:37 +0900

    spring Ex

--- SpringJex01/src/main/java/org/zerock/config/RootConfig.java
+++ SpringJex01/src/main/java/org/zerock/config/RootConfig.java
@@ -2,6 +2,9 @@ package org.zerock.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,14 +12,17 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
+@MapperScan(basePackages= {"org.zerock.mapper"})
 public class RootConfig {
 	
-	//HikariCP¥¬ ∫¸∏• ƒø≥ÿº««Æ¿ª ¡¶∞¯«‘
+	//HikariCPÎäî Îπ†Î•¥Í≥† Ìö®Ïú®Ï†ÅÏù∏ Ïª§ÎÑ•ÏÖòÌíÄÏùÑ Ï†úÍ≥µÌï®
 	@Bean
 	public DataSource dataSource() {
 		HikariConfig hikariConfig = new HikariConfig();
-		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+//		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+//		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE");
 		hikariConfig.setUsername("book_ex");
 		hikariConfig.setPassword("book_ex");
 		
@@ -24,5 +30,31 @@ public class RootConfig {
 		return dataSource;
 	}
 	
-	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		return (SqlSessionFactory)sqlSessionFactory.getObject();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- SpringJex01/src/test/java/org/zerock/persistence/DataSourceTests.java
+++ SpringJex01/src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -1,19 +1,15 @@
 package org.zerock.persistence;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.config.RootConfig;
+import org.zerock.mapper.TimeMapper;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -26,13 +22,52 @@ public class DataSourceTests {
 	@Setter(onMethod_= {@Autowired})
 	private DataSource dataSource;
 	
+	@Setter(onMethod_= {@Autowired})
+	private SqlSessionFactory sqlSessionFactory;
+	
+	@Setter(onMethod_= {@Autowired})
+	private TimeMapper timeMapper;
+	
+//	@Test
+//	public void testConnection() {
+//		try(Connection con = dataSource.getConnection()){
+//			assertNotNull(con);
+//			log.info(con);
+//		}catch(SQLException ex) {
+//			fail(ex.getMessage());
+//		}
+//	}
+	
+//	@Test
+//	public void testMyBatis() {
+//		try(SqlSession session = sqlSessionFactory.openSession(); Connection conn = session.getConnection()){
+//			log.info(session);
+//			log.info(conn);
+//		}catch(SQLException ex) {
+//			fail(ex.getMessage());
+//		}
+//	}
+	
 	@Test
-	public void testConnection() {
-		try(Connection con = dataSource.getConnection()){
-			assertNotNull(con);
-			log.info(con);
-		}catch(SQLException ex) {
-			fail(ex.getMessage());
-		}
+	public void testGetTime2() {
+		log.info("GetTime2");
+		log.info(timeMapper.getTime2());
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
